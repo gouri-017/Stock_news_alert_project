7b526c1c771d1559b1ed8ec747f604c0d1d01b8f
Gouri <80.gouri@gmail.com>
2025-05-03 14:45:02 +0530

    setting up endpoints and parameters

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ import os
 load_dotenv()
 key1_stock_prices = os.getenv("Stock_api")
 key2_news = os.getenv("News_api")
+
+STOCK_NAME = "RELIANCE.BSE"
+COMPANY_NAME = 'RELI'
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://api.marketaux.com/v1/news/all"
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": key1_stock_prices,
+}
+news_params ={
+    "symbols":COMPANY_NAME,
+    'filter_entities':'true',
+    "language":'en',
+    "api_token": key2_news
+}
